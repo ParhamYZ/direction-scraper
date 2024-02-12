d904c08e141cc47f132b9d24e6ee11535d85f84e
parhamyz <0098yazdani@gmail.com>
2024-02-12 17:17:31 +0330

    handle button clicks

--- scraper.py
+++ scraper.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 
-
+# Get selenium chrome web driver
 driver = webdriver.Chrome()
 
-driver.get("https://www.immigration.govt.nz/new-zealand-visas/preparing-a-visa-application/working-in-nz/qualifications-for-work/green-list-occupations")
+driver.get(
+    "https://www.immigration.govt.nz/new-zealand-visas/preparing-a-visa-application/working-in-nz/qualifications-for-work/green-list-occupations"
+)
 
-name_list = driver.find_elements("xpath", "//h3[contains(@class, 'content_list_heading')]")
-for name in name_list:
-    print(name.text)
+# Click on "See More" button until the button is NOT displayed, for seeing all 193 jobs
+btn = driver.find_element("xpath", "//button[contains(@class, 'results_loadbtn')]")
+while btn.is_displayed():
+    driver.execute_script("arguments[0].click();", btn)
+    btn = driver.find_element("xpath", "//button[contains(@class, 'results_loadbtn')]")
 
-# driver.quit()
+# Scrape all job titles
+job_titles = driver.find_elements(
+    "xpath", "//h3[contains(@class, 'content_list_heading')]"
+)
+
+# Write to file
+with open("new_zealand_jobs_list", "w") as file:
+    for title in job_titles:
+        file.write(title.text + "\n")
